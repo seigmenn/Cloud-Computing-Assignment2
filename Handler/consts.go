6eeec350d1d82968b793d0593ed2fae277f1197b
Lars Martin Haugland <larmha@stud.ntnu.no>
2023-04-12 16:50:30 +0200

    Added .csv data file

--- Handler/consts.go
+++ Handler/consts.go
@@ -1,9 +1,9 @@
 package Handler
 
-const LINEBREAK = "\n"                                                 //Line break
-const BASEPATH = "/energy/v1"                                          //Base path of webservice
-const RENEWABLESPATH = BASEPATH + "/renewables"                        //Path to renewables endpoint
-const NOTIFICATIONSPATH = BASEPATH + "/notifications"                  //Path to notifications endpoint
-const STATUSPATH = BASEPATH + "/status"                                //Path to status endpoint
-const COUNTRIESAPIURLALPHA = "http://129.241.150.113:8080/v3.1/alpha/" //URL of Countries API for search with alphacode
-const COUNTRIESAPIURL = "http://129.241.150.113:8080"                  //URL to root of Countries API for status endpoint
+const LINEBREAK = "\n"                                              //Line break
+const BASEPATH = "/energy/v1"                                       //Base path of webservice
+const RENEWABLESPATH = BASEPATH + "/renewables"                     //Path to renewables endpoint
+const NOTIFICATIONSPATH = BASEPATH + "/notifications"               //Path to notifications endpoint
+const STATUSPATH = BASEPATH + "/status"                             //Path to status endpoint
+const COUNTRIESAPIALPHA = "http://129.241.150.113:8080/v3.1/alpha/" //URL of Countries API for search with alpha-code
+const COUNTRIESAPIURL = "http://129.241.150.113:8080"               //URL to root of Countries API for status endpoint
