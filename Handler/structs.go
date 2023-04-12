cbddf988dcb9d17c7382a77153bebeab0f3cc1db
seigmann <aurora@skomsvold.net>
2023-04-12 17:16:37 +0200

    Added struct for renewables percentages handler

--- Handler/structs.go
+++ Handler/structs.go
@@ -1,6 +1,15 @@
 package Handler
 
-import "time"
+import (
+	"time"
+)
 
 // Set startpoint for figuring out elapsed uptime
 var StartTime = time.Now()
+
+type Country struct {
+	Name       string `json:"name"`
+	ISO        string `json:"isocode"`
+	Year       int    `json:"year"`
+	Percentage int    `json:"percentage"`
+}
